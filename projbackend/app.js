8054874a995dfe12eb52504e7c015ec82a1f3303
illusionrs <rajeevshukla255@gmail.com>
2020-07-29 14:46:35 +0530

    updated app.js

---
+++ projbackend/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config()  // for process.env
+
+
+
+const mongoose = require("mongoose");
+const express = require("express")
+
+
+const app = express()
+
+
+mongoose.connect(process.env.DATABASE, 
+{useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true})
+.then(()=> console.log("DB CONNECTED"));
+
+
+const port = 8000;
+
+app.listen(port, ()=> console.log("app is running"));
