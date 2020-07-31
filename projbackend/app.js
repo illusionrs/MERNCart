094d33ecede558bcd6a8e5a21a88d27986df4220
illusionrs <rajeevshukla255@gmail.com>
2020-08-01 00:05:04 +0530

    user auth route added

--- projbackend/app.js
+++ projbackend/app.js
@@ -4,11 +4,18 @@ require("dotenv").config()  // for process.env
 
 const mongoose = require("mongoose");
 const express = require("express")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth")
 
 
 const app = express()
 
 
+
+//DB connected
 mongoose.connect(process.env.DATABASE, 
 {useNewUrlParser: true,
 useUnifiedTopology: true,
@@ -17,5 +24,16 @@ useCreateIndex: true})
 
 
 const port = 8000;
+
+//middleware
+
+app.use(bodyParser.json());
+app.use(cookieParser())
+app.use(cors())
+
+//Routes
+
+app.use("/api",authRoutes)
+
 
 app.listen(port, ()=> console.log("app is running"));
